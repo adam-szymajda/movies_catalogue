afe4ecf5c5268500be8267ed0582c777a4984932
Adam Szymajda <adam.szymajda@eu.equinix.com>
2020-11-05 18:27:14 +0100

    Fixes

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import tmdb_client
+import time
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    movies = ["Film1", "Film2", "Film3", "Film4", "Film5", "Film6", "Film7", "Film8", "Film9", "Film10"]
-    return render_template("homepage.html", movies=movies, color="bg-danger")
+    count = 8
+    list_types = ["latest", "now_playing", "popular", "top_rated", "upcoming"]
+    selected_list = request.args.get('list_name', "popular")
+    movies = tmdb_client.get_movie_lists(count, list_name=selected_list)
+    return render_template("homepage.html", movies=movies, list_types=list_types)
 
+
+@app.route("/movie/<movie_id>/")
+def movie_details(movie_id):
+    movie = tmdb_client.get_single_movie(movie_id)
+    movie['backdrop_path'] = tmdb_client.get_random_backdrop(movie_id)
+    movie_cast = tmdb_client.get_single_movie_cast(movie_id)
+    return render_template("movie_details.html", movie=movie, movie_cast=movie_cast)
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    return {'tmdb_image_url': tmdb_image_url}
+
+@app.context_processor
+def utility_processor():
+    def age_category(movie):
+        if movie.get('adult'):
+            return "bg-danger"
+        return "bg-success"
+    return {'age_category': age_category}
+
+@app.context_processor
+def utility_processor():
+    def url_for_list_type(list_type):
+        return f'url_for("homepage", list_name="{list_type}")'
+    return {'url_for_list_type': url_for_list_type}
 
 
 if __name__ == "__main__":
---
+++ tmdb_client.py
@@ -0,0 +1,56 @@
+import requests
+import random
+
+API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NDI0ZTdhMTBhZDBiMmQ4ZDFlYzg5ZjMwZDFmNWQ4NiIsInN1YiI6IjVmYTE4ZDlmNjkxY2Q1MDAzNmNlMzYwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jQq0KYdq0F6vc9XZXu9_pDocr2A_4uCZsR2lVmozQMQ"
+
+def get_popular_movies(limit):
+    ENDPOINT = "https://api.themoviedb.org/3/movie/popular"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(ENDPOINT, headers=headers)
+    randomised_response = random.sample(response.json()['results'], k=limit)
+    return randomised_response
+    # return response.json()['results'][:limit]
+
+def get_movie_lists(limit, list_name):
+    ENDPOINT = f"https://api.themoviedb.org/3/movie/{list_name}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(ENDPOINT, headers=headers)
+    response.raise_for_status()
+    randomised_response = random.sample(response.json()['results'], k=limit)
+    return randomised_response
+    # return response.json()['results'][:limit]
+
+def get_single_movie(movie_id):
+    ENDPOINT = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    return requests.get(ENDPOINT, headers=headers).json()
+
+
+def get_single_movie_cast(movie_id):
+    ENDPOINT = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    return requests.get(ENDPOINT, headers=headers).json()['cast']
+
+def get_poster_url(poster_path, size='w342'):
+    base_url = "https://image.tmdb.org/t/p/"
+    return f"{base_url}{size}/{poster_path}"
+
+def get_random_backdrop(movie_id):
+    ENDPOINT = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(ENDPOINT, headers=headers)
+    randomised_response = random.choice(response.json()['backdrops'])
+    return randomised_response['file_path']
+
+
+# print(get_popular_movies(1))
